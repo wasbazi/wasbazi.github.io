7533e0fad995d264e24a90ed4a4241727e756dfe
Avi Weinstock-Herman <wasbazi@gmail.com>
2018-02-25 10:44:05 -0700

    Fix issues from wayback machine

--- js/script.js
+++ js/script.js
@@ -18,16 +18,7 @@ $(function() {
     });
     console.log("SPECIAL THANKS:", '\n',
                 "A special thanks goes to a few people who have created free resources:", '\n',
-                "http://web.archive.org/web/20170929200701/http://startbootstrap.com/ for creating and allowing use of the grayscale theme", '\n',
+                "http://startbootstrap.com for creating and allowing use of the grayscale theme", '\n',
                 "as well as http://subtlepatterns.com for the textured paper pattern")
 });
 
-
-/*
-     FILE ARCHIVED ON 20:07:01 Sep 29, 2017 AND RETRIEVED FROM THE
-     INTERNET ARCHIVE ON 06:29:53 Feb 14, 2018.
-     JAVASCRIPT APPENDED BY WAYBACK MACHINE, COPYRIGHT INTERNET ARCHIVE.
-
-     ALL OTHER CONTENT MAY ALSO BE PROTECTED BY COPYRIGHT (17 U.S.C.
-     SECTION 108(a)(3)).
-*/
