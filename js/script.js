0065bd5730b60ee78d4ca173f7b2e6b5e7ab86be
Avi Weinstock-Herman <wasbazi@gmail.com>
2018-02-25 11:16:01 -0700

    Updates to load resources

--- js/script.js
+++ js/script.js
@@ -1,24 +1,27 @@
 //jQuery to collapse the navbar on scroll
 $(window).scroll(function() {
-    if ($(".navbar").offset().top > 50) {
-        $(".navbar-fixed-top").addClass("top-nav-collapse");
-    } else {
-        $(".navbar-fixed-top").removeClass("top-nav-collapse");
-    }
+  if ($(".navbar").offset().top > 50) {
+      $(".navbar-fixed-top").addClass("top-nav-collapse");
+  } else {
+      $(".navbar-fixed-top").removeClass("top-nav-collapse");
+  }
+  var today = new Date()
+  $('#year').text(today.getFullYear())
+
 });
 
 //jQuery for page scrolling feature - requires jQuery Easing plugin
 $(function() {
-    $('.page-scroll a').bind('click', function(event) {
-        var $anchor = $(this);
-        $('html, body').stop().animate({
-            scrollTop: $($anchor.attr('href')).offset().top
-        }, 1500, 'easeInOutExpo');
-        event.preventDefault();
-    });
-    console.log("SPECIAL THANKS:", '\n',
-                "A special thanks goes to a few people who have created free resources:", '\n',
-                "http://startbootstrap.com for creating and allowing use of the grayscale theme", '\n',
-                "as well as http://subtlepatterns.com for the textured paper pattern")
+  $('.page-scroll a').bind('click', function(event) {
+    var $anchor = $(this);
+    $('html, body').stop().animate({
+      scrollTop: $($anchor.attr('href')).offset().top
+    }, 1500, 'easeInOutExpo');
+    event.preventDefault();
+  });
+  console.log("SPECIAL THANKS:", '\n',
+              "A special thanks goes to a few people who have created free resources:", '\n',
+              "http://startbootstrap.com for creating and allowing use of the grayscale theme", '\n',
+              "as well as http://subtlepatterns.com for the textured paper pattern")
 });
 
